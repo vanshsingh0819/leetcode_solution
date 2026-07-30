59cf71139f16a146625570375feefd0019a8e51a
Vansh Singh <vanshsingh0819@gmail.com>
2026-07-30 19:56:51 +0530

    Time: 505 ms (51.28%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
 select name 
 from customer
-where referee_id  is null 
+where referee_id  is null  -- sql by deafault null value ko return nhi krrta hai                      isliye hmme yeh condition likhni pdhi
     or referee_id  != 2;
+    -- IFNULL(referee_id,0) <> 2;  where <> ans != are same
