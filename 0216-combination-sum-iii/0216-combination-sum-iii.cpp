d7ee1f44f438960ec922acdacf845b155b1c5eeb
Vansh Singh <vanshsingh0819@gmail.com>
2026-08-16 21:40:44 +0530

    Time: 2 ms (18.89%), Space: 8.7 MB (61.28%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    void fn(int idx,int k, int n,vector<int> &ds,vector<vector<int>>&ans){
-        if(n == 0 && ds.size() == k){
+    void fn(int idx,int k, int n,vector<int>& ds,vector<vector<int>> &ans){
+        if(n == 0 && k == ds.size()){
             ans.push_back(ds);
-            return;
         }
-       if(n < 0) return;
-        for(int i=idx;i<=9;i++){
+        if(n<0) return;
+        for(int i =idx;i<=9;i++){
             ds.push_back(i);
-            // n-=i;
             fn(i+1,k,n-i,ds,ans);
-            // n+=i;
             ds.pop_back();
         }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int>  ds;
+        vector<int> ds;
         vector<vector<int>> ans;
-        fn(1,k,n ,ds,ans);
+        fn(1,k,n,ds,ans);
         return ans;
     }
 };
