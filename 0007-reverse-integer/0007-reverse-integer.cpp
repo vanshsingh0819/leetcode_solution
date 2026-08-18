51532a902471fa083442e5b5be29b82a253a9efa
Vansh Singh <vanshsingh0819@gmail.com>
2026-08-19 02:44:18 +0530

    Time: 0 ms (100%), Space: 8.5 MB (83.88%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int reverse(int x) {
-         long rev = 0;
-
-        while(x != 0){
+        long rev = 0;
+        while(x > 0){
+            long digit = x % 10;
+            x = x / 10;
+            rev = rev * 10 + digit;
+        }
+        while(x < 0){
             long digit = x % 10;
             x = x / 10;
             rev = rev * 10 + digit;
