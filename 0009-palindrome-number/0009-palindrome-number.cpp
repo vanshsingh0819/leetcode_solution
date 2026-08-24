c3937fd1da79dc4898bcc3e4abd3f8036083dd68
Vansh Singh <vanshsingh0819@gmail.com>
2026-08-24 22:14:50 +0530

    Time: 2 ms (50.12%), Space: 8.6 MB (36.1%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        
-    }
+        if(x < 0){
+            return false;
+        }
+        int n=x;
+        long reverseano = 0;
+        while(x>0){
+            int lastdigit = x%10;
+            x=x/10;
+            reverseano =  (reverseano * 10) +lastdigit;
+            }
+            return n == reverseano;
+        }
 };
