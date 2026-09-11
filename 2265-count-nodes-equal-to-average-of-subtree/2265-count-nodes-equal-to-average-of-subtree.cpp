5cced3eae19407ab491de1df6c0f52db1bd8d79b
Vansh Singh <vanshsingh0819@gmail.com>
2026-09-11 12:46:21 +0530

    Time: 0 ms (100%), Space: 15.8 MB (64.58%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -11,28 +11,20 @@
  */
 class Solution {
 public:
-    int sum(TreeNode* root){
-        if(root == NULL) return 0;
-        int nodesum = root->val + sum(root->left) + sum(root->right);
-        return nodesum;
-    }
-    int cnt(TreeNode* root){
-        if(root == NULL) return 0;
-        int count = 1 + cnt(root->left) + cnt(root->right);
-        return count;
-    }
-    int averageOfSubtree(TreeNode* root) {
-        if(root == NULL) return 0;
-        int ans = 0;
-        int nodesum = sum(root);
-        int nodecount = cnt(root);
-
-        int average = nodesum/nodecount;
-        if(root->val == average){
+int ans = 0;
+    pair<int,int> fn(TreeNode* root){
+        if(root == NULL) return {0,0};
+        pair<int,int> left = fn(root->left);
+        pair<int,int> right = fn(root->right);
+        int sum = root->val + left.first +right.first;
+        int cnt = 1 + left.second + right.second;
+        if(root->val == sum/cnt){
             ans++;
         }
-        ans = ans + averageOfSubtree(root->left);
-        ans = ans + averageOfSubtree(root->right);
+        return {sum,cnt};
+    }
+    int averageOfSubtree(TreeNode* root){ 
+        fn(root);
         return ans;
     }
 };
