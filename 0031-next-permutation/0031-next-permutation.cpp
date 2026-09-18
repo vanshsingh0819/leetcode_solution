018e5d7ee5ef7c342fec2a22c475bd5ba2d73b74
Vansh Singh <vanshsingh0819@gmail.com>
2026-09-18 06:00:58 +0530

    Time: 0 ms (100%), Space: 15.6 MB (82.66%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -3,13 +3,13 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
         int idx = -1;
-        for(int i = n-2;i>=0;i--){
+        for(int i = n-2;i>=0;i--){   //find breaking point
             if(nums[i]< nums[i+1]){
-                idx = i;
+                idx = i;                // i will be taken 
                 break;
             }
         }
-        if(idx == -1){
+        if(idx == -1){              //if breaking point not find
             reverse(nums.begin(),nums.end());
             return;
         }
@@ -19,7 +19,7 @@ public:
                 break;
             }
         }
-        reverse(nums.begin()+idx +1,nums.end());
+        reverse(nums.begin()+idx +1,nums.end());  // reverse all no. after breaking point
 
     }
 };
