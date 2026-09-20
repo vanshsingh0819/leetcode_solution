a727f7454233fe2c1b86f30007deb2f0bf4803e0
Vansh Singh <vanshsingh0819@gmail.com>
2026-09-20 16:39:27 +0530

    Time: 0 ms (100%), Space: 9.8 MB (15.78%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -4,7 +4,7 @@ public:
         int n = s.size();
         int sum = 0;
         for(int i = 0;i<=n-1;i++){
-            int nums = abs(s[i] - 'a' - 26);
+            int nums = abs(s[i] - 'a' - 26);   // 26-(s[i] - 'a')
             int mult = nums * (i+1);
             sum = sum + mult;
         }
