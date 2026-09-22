f0368cec767595ab781ddde7cb153cfb33b23527
Vansh Singh <vanshsingh0819@gmail.com>
2026-09-22 10:17:18 +0530

    Time: 0 ms (100%), Space: 10.2 MB (6.13%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n =s.size();
+        stack<int> st;
+        for(int i = 0;i<=n-1;i++){
+            if(s[i] == '(' || s[i] == '{' ||s[i] == '['){
+                st.push(s[i]);
+            }
+                     
+            else if(!st.empty() && (s[i] == ')' && st.top() == '(' || 
+               s[i] == '}' && st.top() == '{' ||
+               s[i] == ']' && st.top() =='[')){
+                st.pop();
+            }
+            else {
+                return false;
+            }
+            }
+        return st.empty();
+    }
+};
